906479a8a398fb1bce165ee94f8105f732ae257d
Akshit0703 <akshit.lakhanpal07@gmail.com>
2024-07-19 23:10:09 +0530

    sql query, separate mark_grade for teacher and principal

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -73,12 +73,23 @@ class Assignment(db.Model):
 
 
     @classmethod
-    def mark_grade(cls, _id, grade, auth_principal: AuthPrincipal):
+    def mark_grade_teacher(cls, _id, grade, auth_principal: AuthPrincipal):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
-        assertions.assert_valid(assignment.teacher_id == auth_principal.teacher_id, 'This assignment belongs to some other student')
-        assertions.assert_valid((cls.state == AssignmentStateEnum.SUBMITTED or cls.state == AssignmentStateEnum.GRADED), 'Assignment has to be submiited before grading')
+        assertions.assert_valid(assignment.teacher_id == auth_principal.teacher_id, 'This assignment belongs to some other teacher')
+        assignment.grade = grade
+        assignment.state = AssignmentStateEnum.GRADED
+        db.session.flush()
+
+        return assignment
+
+    @classmethod
+    def mark_grade_principal(cls, _id, grade, auth_principal: AuthPrincipal):
+        assignment = Assignment.get_by_id(_id)
+        assertions.assert_found(assignment, 'No assignment with this id was found')
+        assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')        
+        assertions.assert_true(cls.state != AssignmentStateEnum.DRAFT, 'Assingment cannot be in draft state')
 
         assignment.grade = grade
         assignment.state = AssignmentStateEnum.GRADED
